c646c6d652419883d3d0396017be14073f87a0b5
Steven McCreery <logad8@gmail.com>
2017-11-07 17:39:11 -0500

    added question logic with startover loop

--- cli.js
+++ cli.js
@@ -1,19 +1,54 @@
 
 var letters = require("./letter.js");
 
-var prompt = require("prompt");
+var inquirer = require("inquirer");
 
 var guesses = 9;
 
 var round = 1;
 
+var wrongGuesses = [];
+
 var hangman = {
 
 	start() {
-		console.log("==========================");
-		console.log("Hangman Round: " + round + ".\nYou have " + guesses + " guesses remaining.");
-		console.log("==========================");
-		console.log("Guess a letter ");
+		console.log("=====================================================");
+		console.log("Hangman Round: " + round + ".\nYou have " + guesses + " guesses remaining.\nPlease see below for your previous incorrect guesses:\n" + wrongGuesses);
+		console.log("=====================================================");
+		inquirer.prompt([
+			{
+				type: "input",
+				message: "Please guess a letter",
+				name: "letter"
+			}
+		]).then(function(currentGuess){
+			wrongGuesses.push(currentGuess.letter);
+			guesses--;
+			round++;
+			if (guesses > 1) {
+
+				hangman.start();
+			} else {
+				inquirer.prompt([
+					{
+						type: "confirm",
+						message: "Would you like to play again?",
+						name: "confirm",
+						default: true
+					}
+				]).then(function(startOver){
+					if (startOver.confirm) {
+						guesses = 9;
+						round = 1;
+						wrongGuesses = [];
+						hangman.start();
+					} else{
+						console.log("Thanks for playing, goodbye!");
+					}
+				})
+			}
+			// console.log(currentGuess);
+		})
 	}
 
 
